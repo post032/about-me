ebe7ac424554da89afcb5304805f9d92f5d6525c
Elena Ordina <post032@yandex.ru>
2020-11-27 23:29:36 +0300

    dist

---
+++ dist/js/main.js
@@ -0,0 +1,82 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./styles/style.less":
+/*!***************************!*\
+  !*** ./styles/style.less ***!
+  \***************************/
+/*! namespace exports */
+/*! exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./styles/style.less?");
+
+/***/ }),
+
+/***/ "./js/index.js":
+/*!*********************!*\
+  !*** ./js/index.js ***!
+  \*********************/
+/*! namespace exports */
+/*! exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__, __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_style_less__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../styles/style.less */ \"./styles/style.less\");\n\n\nconst HEADER = document.querySelector('.header');\nconst MAIN = document.querySelector('main');\n\nlet burger = HEADER.querySelector('.top__button');\n\nlet onShowMenu = () => {\n    let menu = HEADER.querySelector('.menu');\n    menu.classList.toggle('menu__toggle--show');\n    menu.classList.toggle('menu__toggle--hidden');\n    burger.classList.toggle('top__button--close')\n}\n\nburger.addEventListener('click', onShowMenu);\n\nlet techButton = MAIN.querySelector('.tech__button');\n\nlet onRandColor = function(elem) {\n    let r = Math.floor(Math.random() * (256)),\n        g = Math.floor(Math.random() * (256)),\n        b = Math.floor(Math.random() * (256)),\n        color = '#' + r.toString(16) + g.toString(16) + b.toString(16);\n    return color;\n}\n\nlet onColorTextRange = function () {\n    let techElem = MAIN.querySelectorAll('.tech__element span');\n    techElem.forEach((elem) => {\n        elem.style.color = onRandColor(elem);\n    })\n}\n\ntechButton.addEventListener('click', onColorTextRange);\n\n\n//# sourceURL=webpack:///./js/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	// startup
+/******/ 	// Load entry module
+/******/ 	__webpack_require__("./js/index.js");
+/******/ 	// This entry module used 'exports' so it can't be inlined
+/******/ })()
+;
